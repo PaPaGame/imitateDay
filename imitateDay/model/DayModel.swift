2f89a4ba2061030344fe43b37d277a26c005dc79
振亮 孙 <solszl@163.com>
2015-12-18 17:31:54 +0800

    + 添加数据模型

---
+++ imitateDay/model/DayModel.swift
@@ -0,0 +1,35 @@
+//
+//  DayModel.swift
+//  imitateDay
+//
+//  Created by 振亮 孙 on 15/12/17.
+//  Copyright © 2015年 papa.studio. All rights reserved.
+//
+
+import Foundation
+
+class DayModel: NSObject {
+    
+    var msg: String?
+    
+    var code: Int?
+    
+    var list:[Day]?
+    
+    class func loadData(complete: (data:DayModel, error:NSError?) -> ()) {
+        
+    }
+}
+
+class Day: NSObject {
+    var events:[EventModel]?
+    var themes:[ThemeModel]?
+}
+
+class EventModel: NSObject {
+    
+}
+
+class ThemeModel: NSObject {
+    
+}
